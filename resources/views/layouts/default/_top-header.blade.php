9ef9e6ee7f355ee6bc85b02238b54fc436026f71
kevin89t <kevin@bahaso.com>
2017-06-16 09:43:50 +0700

    Update _top-header.blade.php
    change the date

--- resources/views/layouts/default/_top-header.blade.php
+++ resources/views/layouts/default/_top-header.blade.php
@@ -1,6 +1,6 @@
 <div id="st_top_header" class="nav-utility">
     <div class="module left">
         <i class="ti-location-arrow">&nbsp;</i>
-        <span class="sub">IMF-World Bank Annual Meetings 2018 in Bali, Indonesia, October 14-20, 2018</span>
+        <span class="sub">IMF-World Bank Annual Meetings 2018 in Bali, Indonesia, October 8-14, 2018</span>
     </div>
 </div>
