5b86aa03ccfc5fc99f63ee6952d64937b4d3db7d
Darwin wu <darkeusdj@gmail.com>
2017-08-21 23:24:50 +0800

    add media contact to press release

--- resources/views/press-releases/index.blade.php
+++ resources/views/press-releases/index.blade.php
@@ -19,19 +19,19 @@
 
 @section( 'rhs_content' )
 
-<section class="wysiwyg">
-    <h1 class="title fw600 no-mb">
-        Press Releases
-    </h1>
-    <div class="content">
-        <table class="table am2018">
-            <thead>
+    <section class="wysiwyg">
+        <h1 class="title fw600 no-mb">
+            Press Releases
+        </h1>
+        <div class="content">
+            <table class="table am2018">
+                <thead>
                 <tr>
                     <th width="20%">Date</th>
                     <th>Title</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 <tr>
                     <td>July 12-13, 2017</td>
                     <td>
@@ -44,7 +44,8 @@
                     <td>Juni 13, 2017</td>
                     <td>
                         <a href="{{ route( 'f.press-releases.detail', [2, 'indonesia-menjadi-tuan-rumah' ]) }}">
-                            Indonesia Menjadi Tuan Rumah Pertemuan Ekonomi dan Keuangan Terbesar di Dunia: IMF-World Bank Annual Meetings 2018
+                            Indonesia Menjadi Tuan Rumah Pertemuan Ekonomi dan Keuangan Terbesar di Dunia: IMF-World
+                            Bank Annual Meetings 2018
                         </a>
                     </td>
                 </tr>
@@ -56,10 +57,19 @@
                         </a>
                     </td>
                 </tr>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
 
-    </div>
-</section>
+        </div>
+
+        <h1 class="title fw600 no-mb">
+            Media Contact
+        </h1>
+        <div class="content">
+            Indonesia Planning Team <br>
+            Telp: +62-21-3849315, +62-21-29815250 <br>
+            Email: am_2018@kemenkeu.go.is, AM2018_BI@bi.go.id <br>
+        </div>
+    </section>
 
 @stop
