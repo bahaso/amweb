268ba1382c13940841513e1dd21c76429462c4c0
kevin <kevin@bahaso.com>
2017-08-22 14:40:50 +0700

    toursism packages

--- resources/views/discovers/tourism.blade.php
+++ resources/views/discovers/tourism.blade.php
@@ -28,7 +28,7 @@
     <div class="mix-color-title mb-50">
         <span class="mix-color-bar left" style="width:170px;margin-top:23px;"></span>
         <span class="mct-row fm-signika-reg sm-font">
-            Tourism Package
+            Tourism Packages
         </span>
         <span class="mix-color-bar right" style="width:170px;margin-top:23px;"></span>
     </div>
@@ -37,7 +37,7 @@
         As Indonesia has large area and various traditions to explore, it might be hard for travellers to find the best itinerary 
         in a limited period. Wonderful Indonesia proudly presents to you some of the best tourism packages from our certified partners 
         around the areas on Indonesia for your options. You can also read our articles for further information before you decide which
-        packages to accompany you on your dream holiday!
+        packages to accompany you on your journey.
     </p>
 
     <div class="mix-bar-full no-mb"></div>
