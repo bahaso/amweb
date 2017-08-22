e78850a62fa4c5406a66deb3a5d851ba7745d10b
kevin <kevin@bahaso.com>
2017-08-22 19:01:11 +0700

    Travel Tips

--- resources/views/welcomes/tips.blade.php
+++ resources/views/welcomes/tips.blade.php
@@ -21,7 +21,7 @@
 
 <section class="wysiwyg">
 	<h1 class="title fw600">
-		Essential Bali travel tips: what to know before you go
+		Essential Travel Tips: What to Know Before You Go
 	</h1>
 	<div class="content">
 		<p>
@@ -29,157 +29,74 @@
 		</p>
 
 		<h3 class="in-title">
-			Expect crowds
+			Language
 		</h3>
-		<p>
+		<p>We recommend learning a few phrases in Bahasa Indonesia.<br>
+		Selamat pagi (good morning)<br>
+		Tolong (please)<br>
+			Terima kasih (thank you)<br></p>
+		{{--<p>
 			One of the most touristed islands on earth, Bali isn’t exactly an untouched paradise. 
 			But while it can be difficult to escape the throngs of <span class="txt-underline">south Bali</span> and 
 			<span class="txt-underline">Ubud</span>, determined solitude seekers 
 			will be pleased to find loads of secluded corners beyond these primary tourist centres. Tip: head to the central mountains, 
 			or Bali’s more chilled-out north and west coasts.
-		</p>
+		</p>--}}
 		<h3 class="in-title">
-			Choose your base carefully
+			General Information
 		</h3>
 		<p>
-			It pays to put some thought into your Bali base, 
-			as chaotic traffic and hot weather are likely to make you stick close to your hotel or guesthouse rather
-			than wander far on foot or sit in stuffy taxis. If you’re looking for real R&R, <span class="txt-underline">Kuta</span> probably isn’t your thing. 
-			If you want to shop up a storm and eat more than your body weight in fine food, a week on <span class="txt-underline">Nusa Lembongan</span> isn't likely
-			to leave you fully satiated. 
+			Office hours for most businesses are from 8 AM to 4 PM, or 9 AM to 5 PM, usually Monday through Friday (with some open on Saturday as well). Government offices are closed on Saturdays. Standard banking hours are from 8 AM to 3 PM, Monday to Friday.
 		</p>
 		<h3 class="in-title">
-			Don’t fret about 'Bali belly'
+			Currency
 		</h3>
 		<p>
-			Strict dietary habits are no longer required to prevent spending your Bali break within two steps of a toilet. Once upon a time, salads, cut fruit, ice cubes and most meats were on the danger list, but hygiene standards have improved markedly across the island, and many kitchens offer good quality organic produce. While dodgy prawns will always be out there, by staying hydrated, avoiding notorious local liquor arak and consuming street food with a degree of caution, the dreaded Bali belly should be kept at bay.
+			The Indonesian currency is Indonesia Rupiah (IDR). Money exchange facilities are widely available and information on the daily exchange rate can be found <a href="http://www.bi.go.id/en/moneter/informasi-kurs/transaksi-bi/Default.aspx" target="_blank">online</a>. It is recommended to exchange money at commercial banks or registered money changers. Credit cards are widely accepted in hotels, restaurants, and most shops/vendors. Most ATMs will take non-Indonesian cards, although out-of-network fees will apply. Most ATMs dispense Rupiah only, and it comes in either Rp 50,000 and 100,000 notes.
 		</p>
 
 		<h3 class="in-title">
-			Dress for the occasion
+			Electricity
 		</h3>
 		<p>
-			Beachwear doesn’t always cut it in Bali – many higher-end bars, restaurants and clubs enforce a dress code. If you’re unsure, call ahead to save the potential embarrassment of being turned away.
+			Electric power supply is 220 volts in all regions (110-volt electronic equipment will need a converter and an adaptor). Sockets fit two pin, rounded-tip plugs (technically known as Type C, E, and F). (ADD PHOTO) Some hotels are outfitted with multiple sockets but it is best to travel with an adaptor.
 		</p>
 
 		<h3 class="in-title">
-			Respect religious customs
+			Internet/WiFi
 		</h3>
 		<p>
-			Religion rules the roost in Bali. Don’t get your knickers in a knot when a street is blocked off for a ceremony or your driver pulls over mid-trip to make a blessing – this is all part of the magic of the island. Plan accordingly if your travel dates fall on <span class="txt-underline">Nyepi</span> when everything in Bali (even the airport) shuts down for the day, and always dress modestly (covering the shoulders and knees) and conduct yourself appropriately when visiting temples and holy sites.
+			All hotels and most restaurants provide internet connections or free WiFi.
 		</p>
 
 		<h3 class="in-title">
-			Prepare for a mixed bag of price tags
+			Embassies/Consulates
 		</h3>
 		<p>
-			It’s still possible to visit Bali on a shoestring by staying in guesthouses, 
-			dining at <span class="txt-italic">warungs</span> and shopping at local markets, 
-			but you can just as easily blow your life savings as drinks, meals, 
-			spa treatments and room rates at high-end establishments are priced similarly to that in Australia, 
-			the UK and the US. Look out for online discounts and happy hour deals.
-		</p>
-
-				<h3 class="in-title">
-			Expect crowds
-		</h3>
-		<p>
-			One of the most touristed islands on earth, Bali isn’t exactly an untouched paradise. 
-			But while it can be difficult to escape the throngs of <span class="txt-underline">south Bali</span> and 
-			<span class="txt-underline">Ubud</span>, determined solitude seekers 
-			will be pleased to find loads of secluded corners beyond these primary tourist centres. Tip: head to the central mountains, 
-			or Bali’s more chilled-out north and west coasts.
+			For a list of consulates in Bali and their contact details please refer to <a href="http://www.balitourismboard.org/consulates_in_bali.html" target="_blank">http://www.balitourismboard.org/consulates_in_bali.html</a>.
 		</p>
 
 		<h3 class="in-title">
-			Be cautious of wild and stray animals
+			Shopping
 		</h3>
 		<p>
-			Give wild and stray animals a wide berth. They may look cute, but rabies and other diseases are serious risks in Bali and monkeys are notorious for their thieving ways. Bali’s stray dogs are numerous, and often in pretty bad shape. 
+			In traditional markets and individual stalls, it is an acceptable custom to negotiate the final price with the vendor if the price is not clearly marked on the item. But always do so respectfully and pleasantly. In larger stores or chains, the price is fixed.
 		</p>
 
 		<h3 class="in-title">
-			Avoid plastic water bottles
+			Transportation
 		</h3>
 		<p>
-			Bali’s heat and humidity calls for constant hydration, but consider the environment before purchasing another bottled drink. An estimated three million plastic bottles are discarded in Bali each month; help reduce this figure by investing in a stainless steel bottle that you can refill; most good cafes and restaurants have a water filter available that you can use for free or for a small fee. 
+			Taxis are widely available in and around the Nusa Dua area and can be arranged through hotels, the meetings venues, or directly with local taxi companies.
 		</p>
 
 		<h3 class="in-title">
-			Learn some local lingo
+			Weather
 		</h3>
 		<p>
-			A few basic words of Bahasa Indonesia will take you a long way in Bali. 
-			Try <span class="txt-italic">selamat pagi</span> (good morning), <span class="txt-italic">tolong</span> (please) 
-			and <span class="txt-italic">terima kasih</span> (thank you), for starters.
-		</p>
-
-		<h3 class="in-title">
-			Remember that low season often means rainy season
-		</h3>
-		<p>
-			Be mindful of Bali’s rainy season (January to April and October to November) when planning your trip. Discounts can be great, but if you end up spending your holiday cooped up indoors, you may be left wondering if making the trip was worth it. Fortunately, the rains are often limited to brief afternoon downpours, so your holiday isn't likely to be a total write-off.
-		</p>
-
-		<h3 class="in-title">
-			Bargain respectfully
-		</h3>
-		<p>
-			You can bargain for many items and services in Bali, but do so respectfully and with a smile on your face. You’ll know when the vendor has reached their limit, and at that point don’t push it. When in doubt, walk away – if the seller doesn’t come after you, you can be sure they aren’t prepared to drop their price any lower.
-		</p>
-
-		<h3 class="in-title">
-			Get your head around the current visa situation
-		</h3>
-		<p>
-			In early 2015, <span class="txt-underline">Indonesia</span> waived its standard 30-day tourist visa-on-arrival (VOA) system for 45 countries; 
-			visitors from most other nations (including Australians) must purchase the VOA. While extending a 30-day visa is possible, it can be a tricky business. Speak to a reputable visa agent on the ground, or contact your nearest Indonesian consulate prior to departure. At the time of publication, 60-day visas could be arranged in advance, but not in-country.
-		</p>
-
-		<h3 class="in-title">
-			Play by the rules
-		</h3>
-		<p>
-			The Indonesian legal system may seem confusing and contradictory, but it's best not argue with police if you are accused of an infringement that may feel unjust, and pay ‘fines’ with good grace. 
-		</p>
-
-		<h3 class="in-title">
-			Respect the ocean
-		</h3>
-		<p>
-			Even if you’re an avid beach-goer and surf worshipper, Bali's powerful waves, strong currents and exposed rocks can be treacherous, so take care, and don't swim alone unless you are completely confident in doing so. Show equal respect for the beach by not leaving any garbage (including cigarette butts) behind – when the tide comes in, it'll be sucked into the ocean at great cost to the marine ecosystem.
-		</p>
-
-		<h3 class="in-title">
-			Don’t stress
-		</h3>
-		<p>
-			Bali is generally safer than the headlines suggest, but with close to four million tourists hitting its shores every year, it’s statistically natural that some travellers may have problems. Party safe, always wear a helmet when riding a bike or scooter, be respectful, and don’t do anything you wouldn’t do in your home country, and you're on track for the holiday of a lifetime.
-		</p>
-
-		<p>
-			<span class="txt-italic">Written by Samantha Chalker, (Lonely Planet Writer), originally published in July 2009, and updated in August 2012 and July 2015.</span>
-			<a href="https://www.lonelyplanet.com/travel-tips-and-articles/essential-bali-travel-tips-what-to-know-before-you-go/40625c8c-8a11-5710-a052-1479d277522a">https://www.lonelyplanet.com/travel-tips-and-articles/essential-bali-travel-tips-what-to-know-before-you-go/40625c8c-8a11-5710-a052-1479d277522a
-			</a>
-		</p>
-	</div>
-
-	<h1 class="title fw600">
-		Indonesia Weather
-	</h1>
-	<div class="content">
-		<p>
-			Indonesia’s climate is relatively even year-round as the country lies along the equator. Indonesia has two seasons –a wet season and a dry season– with no extremes temperature between the two seasons. The average temperature in most of Indonesia region is between 20˚ - 36˚C depending on the region’s altitude.
-		</p>
-		<p>
-			For most of Indonesia’s area, the dry season falls between April and October with the wet season between November and March. This cycle, however, is relatively unstable in the last few years due to climate change.
-		</p>
-		<p>
-			Indonesia’s climate is almost entirely tropical, dominated by the Tropical rainforest climate found in every major island of Indonesia, followed by the Tropical monsoon climate that predominantly lies along Java’s coastal north, Sulawesi’s coastal south and east, and Bali, and finally the tropical Savana climate, can be found in isolated location in Central Java, lowland of East Java, coastal southern Papua and smaller islands to the east of Lombok. However, cooler climate types can be found in mountainous regions of Indonesia at 1,300 to 1,500 metres above sea level
-		</p>
-		<p class="no-mb">
-			Further Information regarding Indonesia’s weather can be found at:
-			<a href="https://www.accuweather.com/en/id/indonesia-weather">https://www.accuweather.com/en/id/indonesia-weather</a>
+			Indonesia has two seasons, wet and dry, with no temperature extremes between the two seasons. For most of the country, the dry season falls between April and October and the wet season between November and March. However, this cycle can be unpredictable.<br>
+			<br>Although cooler climate types can be found in mountainous regions of Indonesia at 1,300 to 1,500 meters above sea level, Indonesia’s climate is almost entirely tropical, dominated by the tropical rainforest climate found in every major island of Indonesia, followed by the tropical monsoon climate in other areas, and finally the tropical savanna climate which can be found in isolated locations. Overall, the average temperature in most regions is between 20˚-36˚C depending on the region’s altitude.<br>
+			<br>For more information regarding Indonesia’s weather, please refer to: <a href="https://www.accuweather.com/en/id/indonesia-weather" target="_blank">https://www.accuweather.com/en/id/indonesia-weather</a>.
 		</p>
 	</div>
 </section>
