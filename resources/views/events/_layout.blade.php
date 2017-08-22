dda44381cccaace851f56b6561a736d7423b5c95
Darwin wu <darkeusdj@gmail.com>
2017-08-22 13:50:35 +0800

    fix new content for VTI program

--- resources/views/events/_layout.blade.php
+++ resources/views/events/_layout.blade.php
@@ -11,26 +11,28 @@
             Side Events
         </a>
     </li>
-    <li class="has-children">
+    <li class="has-children {{ isActiveRoute( 'f.events.program_vti' ) }}">
         <a href="{{ route( 'f.events.program_vti' ) }}" class="lbl">
-            Program
+            Voyage To Indonesia (VTI)
         </a>
         <ul>
-            <li class="{{ isActiveRoute( 'f.events.program_vti' ) }}">
-                <a href="{{route( 'f.events.program_vti' )}}" class="lbl">
-                    VTI Program
-                </a>
+            <li>
+                <a href="#" class="lbl">Upcoming VTI Events</a>
             </li>
-            <li class="{{ isActiveRoute( 'f.events.bank' ) }}">
-                <a href="{{route( 'f.events.bank' )}}" class="lbl">
-                    BI
-                </a>
+            <li>
+                <a href="#" class="lbl">Past VTI Events</a>
             </li>
-            <li class="{{ isActiveRoute( 'f.events.ministry' ) }}">
-                <a href="{{route( 'f.events.ministry' )}}" class="lbl">
-                    Ministry of Finance
-                </a>
-            </li>
+
+            {{--<li class="{{ isActiveRoute( 'f.events.bank' ) }}">--}}
+                {{--<a href="{{route( 'f.events.bank' )}}" class="lbl">--}}
+                    {{--BI--}}
+                {{--</a>--}}
+            {{--</li>--}}
+            {{--<li class="{{ isActiveRoute( 'f.events.ministry' ) }}">--}}
+                {{--<a href="{{route( 'f.events.ministry' )}}" class="lbl">--}}
+                    {{--Ministry of Finance--}}
+                {{--</a>--}}
+            {{--</li>--}}
         </ul>
     </li>
 @stop
--- resources/views/events/program-vti.blade.php
+++ resources/views/events/program-vti.blade.php
@@ -19,154 +19,63 @@
 
 @section( 'rhs_content' )
 
-<section class="wysiwyg">
-    <h1 class="title fw600">
-        Voyage To Indonesia Program (Ministry Of Finance)
-    </h1>
-    <div class="content">
-        <section class="faq">
-            <ul class="accordion accordion-2 one-open">
-                <li class="active">
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            1. Workshop of “Improvement of Regional Efficiency and Transparency in Enhancing Public
-                            Service Quality and Attracting Investments in Regional Area”, 21 February 2017 in
-                            Yogyakarta
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            This workshop aims to lay out a variety of data and information on the latest condition of
-                            how regional area implemented its duties and authorities in order to improve regional’s
-                            public service quality in the future, especially in developing government and private
-                            financing innovations.
-                        </p>
-                    </div>
-                </li>
-                    <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            2. Focus Group Discussion (FGD) on “The Rising of a New World Order: Indonesia’s
-                            Challenges and Response”, 30 March 2017 in Bandung
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            This FGD is held in response to global condition in which values and governance upheld by various key countries in the world have shifted, such as the United Kingdom with its Brexit phenomenon and the United States of America with its new policies under the control of President Donald Trump. It has brought some parties to assume that it was the beginning of New World Order. Although it is still debated, but the shift’s significance cannot be denied because of the absorbing counter/narrative movement of the commnunist countries with its strong belief in economic liberalism which tinkering with economic nationalism rhetorics leading to protectionism.
-                        </p>
-                    </div>
-                    </li>
+    <section class="wysiwyg">
+        <h1 class="title fw600">
+            Voyage To Indonesia Program (Ministry Of Finance)
+        </h1>
+        <div class="content">
+            <p>
+                The Voyage to Indonesia program will feature public events, seminars/panels, and publications
+                showcasing Indonesia and ASEAN as a reformed, resilient, progressive, and pro-job economy. VTI
+                events will be integrated under four overarching themes:
+            </p>
+            <ol>
                 <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            3. Roundtable Discussion (RTD): “Indonesia-WB Infrastructure Financing”, 19 April 2017 in Washington DC (WB-IMF Spring Meeting 2017)
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            This activity is held to facilitate and respond to the Government of Indonesia’s ambitious plan related to the country’s infrastructure development. The Minister of Finance of the Republic of Indonesia believes that infrastructure is one way to boost Indonesia’s economic growth by contemplating the possibility of middle income trap, as well as growth target in 2017 amounting to 5.1%. Unfortunately, the plan was conflicted with the Government’s budget.
-                        </p>
-                    </div>
+                    Development
+                    <ul>
+                        <li>
+                            Inclusive Growth - sharia economy and other initiatives
+                        </li>
+                        <li>
+                            Empowerment of women in Indonesia’s economy
+                        </li>
+                    </ul>
                 </li>
+
                 <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            4. National Seminar/Ministry of Finance’s Economists Forum: “Sustainable Growth and Shared Prosperity”, 17-18 May 2017 in Palembang
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            This national seminar is held by the cooperation between MoF and the Regional Economists. The aims of the event is discussing relevant issues on sustainable growth and shared prosperity. The discussion drew on the perspectives of Central Government i.e. Ministry of Finance, regional, academics/public and international.
-                        </p>
-                    </div>
+                    Economy and Finance
+                    <ul>
+                        <li>
+                            Building a reformed and resilient economy which aims to show that Indonesia has
+                            undergone significant reform and is resilient towards external shocks.
+                        </li>
+                        <li>
+                            Digitalized economy, representing Indonesia’s progress in embracing modern technology to
+                            promote economic growth.
+                        </li>
+                    </ul>
                 </li>
-                <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            5. Joint IMF-Indonesia High-Level Conference “International Taxation in Asia: Issue and The Way Forward, 12-13 July in Jakarta
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            Strengthening tax systems has emerged as a key development priority for the implementation of the 2030 Sustainable Development Goals (SDG). It is recognized that strong tax systems are key for enhancing state building; it is not just how much revenue is raised that matters for development and growth, but also how it is raised. The establishment of the ASEAN Economic Community (AEC), along with the expansion of cross-border investment in the ASEAN region, amplifies unwanted side-effects in terms of aggressive tax planning by multinational and regional companies. Simultaneously, the weakness of coordinating efforts by ASEAN tax authorities enables investors to take advantage of that opportunity for tax evasion.
-                            The conference will provide a forum to engage policymakers on issues related domestic resource mobilization, including through international tax reforms. The scope and nature of initiatives to address mismatches and loopholes of the current international tax architecture, remaining areas of difficulty, and the challenges of tax competition and coordination will be addressed.
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            6. Indonesia Infrasructure Finance Forum 2017, 25 July 2017 in Jakarta
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            The need for infrastructure development in Indonesia is massive, partly due to the archipelagic landscape and the huge population, coupled with its steady economic growth.
-                        </p>
-                        <p>
-                            The government of Indonesia under President Jokowi’s administration has consistently made infrastructure development one of its highest priority. However, the capacity of the government to finance the development of infrastructure through the government budget is limited.
-                        </p>
-                        <p>
-                            The forum aims to: 1) Demonstrated achievments by the Indonesian government in developig infrastructure finance; 2) Identify the key future challenges on infrastructure finance, and how those challenges may be solved, or even turned into opportunities, taking into account the input from different perspective of the internal government, development agencies, as well as the private sector; and 3) Explore the opportunities for private sector and multilateral development agencies to take a bigger role in supporting Indonesia’s infrastructure development.
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            7. 2nd Annual Islamic Finance Conference, 23-24 Agustus 2017 in Yogyakarta
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            Socio-economic problems remain the most challenging issue faced by Muslim world, including
-                            Indonesia, during the last decades. The poor socio-economic performance of most Muslim-majority countries has raised a longstanding debate on whether Islam as the “religion of practice” has relationships with socioeconomic growth, or whether there are deeper problems, outside the Islamic values, contributing to lower education, poverty, poor health in the countries, although the Islamic economic principles have been introduced hundreds years ago (Timmer & McClelland 2004).
-                        </p>
-                        <p>
-                            The main objective of the conference is to promote discussion on the application of theory, model and instruments of Islamic finance to overcome socio-economic problems in developing countries including Indonesia. Furthermore, the conference aims to discuss a set of policies combined with the principles of Islamic finance that can be together tackling poverty and income inequality problems.
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            8. APEC Seminar on Disaster Risk Financing in Asia Pacific Region, October 2017 in Padang
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            The severe impact of natural disasters has been widely acknowledged by global world, particularly in regions within the “Ring of Fire”, such as the Asia-Pacific. Large disaster events have caused major human, social, economic and financial impacts. APEC, under Finance Ministers Process (FMP), is one of very active organization which producing disaster related initiatives. Vietnam as APEC Chairman has set the APEC 2017 theme of Creating New Dynamism, Fostering Shared Future; and maintained the Disaster Risk Financing and Insurance (DRFI) as one of four main agendas. Meanwhile, the Indonesian Ministry of Finance as the fiscal authority puts high concern on the fiscal resilience on managing financial risk of disaster. Incorporating Disater Risk Management (DRM)/DRFI into its fiscal sustainability framework is a necessity, taking into account the limited fiscal space.
-                        </p>
-                        <p>
-                            Based on the need to further enhance its policy in disaster risk financing and to align the DRM into fiscal policy and development planing, Indonesia needs to review its related existing policies to determine the suitable policies in the short, medium and long run. Therefore, This seminar is aiming to provide foundation for Indonesia to implementing integrated DRM and fiscal policy in a holistic approach, with a view to reinventing the disaster risk management framework and its prevailing laws and regulations.
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <div class="title">
-                        <h4 class="inline-block mb0">
-                            9. Regional Seminar on Urbanization, December 2017 in Surabaya
-                        </h4>
-                    </div>
-                    <div class="content">
-                        <p>
-                            The level and growth of urbanization differ considerably by region. Among developing countries, Latin American countries have the highest proportion of their population living in urban areas. But East and South Asia are likely to have the fastest growth rates in the next 30 years including in Indonesia.
-                        </p>
-                        <p>
-                            Experience suggests that if well managed, urban areas promote efficiency and productivity increases through agglomeration effects; foster innovation and the emergence of new ideas; provide job opportunities and the growth of the middle class.
-                        </p>
-                        <p>
-                            However, Population growth, especially due to rural-to-urban migration (urbanization) has led to urban densification and urban sprawl. This condition presents a variety of very complex urban problems; include social, economic and environmental issues. Various problems that occur in urban areas because of those condition are the increasing rate of poor people, limited of the availability of employment rate, and increasing demand for housing.
-                        </p>
-                        <p>
-                            This regional seminar aims to deliver the findings of the comprehensive study on Urbanization including best practise program of some regions in Indonesia. It will be a sharing experience and initiatives forum related to urbanization issues especially from other East and South Asia countries.
-                        </p>
-                    </div>
-                </li>
-            </ul>
-            <!--end of accordion-->
-        </section>
-    </div>
-</section>
+                <li>Indonesia’s Diversity</li>
+                <li>Indonesia’s Potential</li>
+            </ol>
+            <p>
+                A wide range of activities such as seminars, focused group discussions (FGD), and workshops will be
+                organized around the first two themes. “Indonesia’s Diversity” will be showcased through cultural
+                events
+                and targeted presentations celebrating the unique heritage and composition of the country. Lastly,
+                “Indonesia’s Potential” will promote programs and priority initiatives for the host country, namely
+                trade fairs and investment opportunities. Several ministries and other respective institutions are
+                collaborating to organize and carry out these events.
+            </p>
+            <p>
+                Aside from the four themes above, VTI will also conduct various outreach programs to stakeholders
+                such
+                as private sector, parliament, youth, academics, media, NGO/think tank, and international community
+                in
+                order to convey the message of Indonesia’s economic achievements and to explain the roles of
+                international organizations, such as the IMF and World Bank Group.
+            </p>
+        </div>
+    </section>
 @stop
 
