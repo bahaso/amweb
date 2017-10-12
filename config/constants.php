d31b1c27102122207471d65ea715bdb1a6744ff4
kevin89t <kevin@bahaso.com>
2017-10-12 10:29:53 +0700

    Update Channel ITX

--- config/constants.php
+++ config/constants.php
@@ -6,8 +6,8 @@ return [
     [
         'itx' =>
         [
-            'channel_id'  => 'jk_amimfwb',
-            'channel_key' => '754A6553-3B77-4719-9DC3-34586AAC4FB9'
+            'channel_id'  => 'provider',
+            'channel_key' => '1C156425-4767-4EB5-A345-B40AA8286034'
         ]
     ],
 
